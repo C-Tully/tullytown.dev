fad6c4884b7a01131ac0159e6c5b13e79c4da4f6
Chris Tully <christully12@gmail.com>
2018-02-20 12:11:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tullytown.dev
---
+++ application/views/header/meta.php
@@ -0,0 +1,18 @@
+<!-- Start Meta -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="Welcome to Chris Tully's Digital Portfolio"/>
+<!-- Start OG Values -->
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Chris Tully | Web Developer | Digital Marketer" />
+	<meta property="og:description" content="Welcome to Chris Tully's Digital Portfolio" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="Chris Tully's Digital Portfolio" />
+	<meta name="twitter:card" content="" />
+	<meta name="twitter:description" content="Welcome to Chris Tully's Digital Portfolio" />
+	<meta name="twitter:title" content="Chris Tully | Web Developer | Digital Marketer" />
+	<meta name="twitter:site" content="" />
+<!-- End OG Values -->
