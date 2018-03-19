ed107cb4f75a5bce3a690032edb22fc3e6134375
Chris Tully <christully12@gmail.com>
2018-03-19 11:57:58 -0400

    Favicon and Og Image
    addition of images for OG and favicon

--- application/views/components/header/meta.php
+++ application/views/components/header/meta.php
@@ -9,11 +9,21 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Chris Tully | Web Developer | Digital Marketer" />
 	<meta property="og:description" content="Welcome to Chris Tully's Digital Portfolio" />
-	<meta property="og:url" content="" />
+	<meta property="og:url" content="http://tullytown.org" />
 	<meta property="og:site_name" content="Chris Tully's Digital Portfolio" />
+	<meta property="og:image" content="<?php echo base_url(); ?>/assets/images/blue_code.jpg" />
 	<meta name="twitter:card" content="" />
 	<meta name="twitter:description" content="Welcome to Chris Tully's Digital Portfolio" />
 	<meta name="twitter:title" content="Chris Tully | Web Developer | Digital Marketer" />
 	<meta name="twitter:site" content="" />
 <!-- End OG Values -->
 <!-- End Meta -->
+
+<!---favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>/assets/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>/assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>/assets/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
