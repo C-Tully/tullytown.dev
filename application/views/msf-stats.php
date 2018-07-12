5eea5053c7d286e845c3aa444f835aa8020961e2
Chris Tully <christully12@gmail.com>
2018-07-11 20:16:45 -0400

    update
    UPDATE FOR MSF

---
+++ application/views/msf-stats.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+ <?php //$this->load->view('components/header/head'); ?>
+
+<!DOCTYPE html>
+	<html lang="en"  xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php $this->load->view('components/header/meta'); ?>
+		<?php $this->load->view('components/header/title'); ?>
+            <?php $this->load->view('components/header/gtm_1.js'); ?>
+		<?php $this->load->view('components/header/schema'); ?>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/styles/main_layout.css');?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/styles/style.css');?>">
+		<link href="" rel="stylesheet">
+		<script type="text/javascript" defer="" src="<?php echo base_url('/assets/scripts/core.js');?>"></script>
+	</head>
+
+<body class="msf-background">
+  <?php $this->load->view('components/header/gtm_2.js'); ?>
+
+<div id="container">
+  	  
+	 <div class="quickLink">
+	 	<a href="/#team-one">Team One</a>
+	 	<a href="/#team-two">Team Two</a>
+	 	<a href="/#team-three">Team Three</a>
+	 </div>
+	<table id="team-one">
+		<thead>
+			<th>Name</th>
+			<th>Alpha Team STP</th>
+			<th>Beta Team STP</th>
+			<th>Gamma Team STP</th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+				<tr>
+				<td>KaosAkroma</td>
+				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
+				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
+				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+			</tr>
+
+		</tbody>
+	</table>
+
+  
+</div>
+
