b862e20c4b4220204d6fa1b763804f1fb730680b
Chris Tully <christully12@gmail.com>
2018-07-12 19:24:04 -0400

    Deployment
    Addition of final og values for page, addition of tables addition of css

---
+++ application/views/components/header/meta-msf.php
@@ -0,0 +1,19 @@
+<!-- Start Meta -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="::X-Ternals::"/>
+<!-- Start OG Values -->
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="::X-Ternals::" />
+	<meta property="og:description" content="::X-Ternals::" />
+	<meta property="og:image" content="<?php echo base_url(); ?>assets/images/favicon/marvel_icon.jpg" />
+
+<!-- End OG Values -->
+<!-- End Meta -->
+
+<!---favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>/assets/images/marvel_icon.jpg">
+
--- application/views/msf-stats.php
+++ application/views/msf-stats.php
@@ -7,9 +7,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 	<html lang="en"  xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<?php $this->load->view('components/header/meta'); ?>
+		<meta name="robots" content="noindex">
+		<?php $this->load->view('components/header/meta-msf'); ?>
 		<?php $this->load->view('components/header/title'); ?>
-            <?php $this->load->view('components/header/gtm_1.js'); ?>
+           <?php $this->load->view('components/header/gtm_1.js'); ?>
 		<?php $this->load->view('components/header/schema'); ?>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/styles/main_layout.css');?>">
@@ -22,61 +23,201 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <?php $this->load->view('components/header/gtm_2.js'); ?>
 
 <div id="container">
-  	  
+  	  <div class="header_img">
+  	  	<img src="/assets/images/xternal-header.png" />
+  	  </div>
+
+
 	 <div class="quickLink">
-	 	<a href="/#team-one">Team One</a>
-	 	<a href="/#team-two">Team Two</a>
-	 	<a href="/#team-three">Team Three</a>
+	 	<a href="/index.php/Msf/#team-one">Team One</a>
+	 	<a href="/index.php/Msf/#team-two">Team Two</a>
+	 	<a href="/index.php/Msf/#team-three">Team Three</a>
 	 </div>
+
+	<div class="team_lead">
+		<h3>Team Leads: </h3>
+		<p>Commander740, </p>
+		<p>imthatguy</p>
+	</div>
 	<table id="team-one">
 		<thead>
-			<th>Name</th>
+			<th>Name</th>			
 			<th>Alpha Team STP</th>
 			<th>Beta Team STP</th>
 			<th>Gamma Team STP</th>
+			<th>Thanos Raid Lane</th>
+			<th>Ulitmis Raid 5 Lane </th>
 		</thead>
 		<tbody>
 			<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>Commander740</td>
+				<td>88886</td>
+				<td>73075</td>
+				<td>62327</td>
+			</tr>
+			<tr>
+				<td>Imthatguy</td>
+				<td>83101</td>
+				<td>76011</td>
+				<td>61670</td>
+				<td></td>
+				<td></td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>Ady</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>King Vic</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>mKizzy</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>DanimePK</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+			<tr>
+				<td>PaulThaAsian</td>
+				<td >82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+
+		</tbody>
+	</table>
+		<div class="team_lead">
+		<h3>Team Leads: </h3>
+		<p>Commander740, </p>
+		<p>imthatguy</p>
+	</div>
+
+	<table id="team-two">
+		<thead>
+			<th>Name</th>			
+			<th>Alpha Team STP</th>
+			<th>Beta Team STP</th>
+			<th>Gamma Team STP</th>
+			<th>Thanos Raid Lane</th>
+			<th>Ulitmis Raid 5 Lane </th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Commander740</td>
+				<td>88886</td>
+				<td>73075</td>
+				<td>62327</td>
+			</tr>
+			<tr>
+				<td>Imthatguy</td>
+				<td>83101</td>
+				<td>76011</td>
+				<td>61670</td>
+				<td></td>
+				<td></td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>Ady</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
 			</tr>
 				<tr>
-				<td>KaosAkroma</td>
-				<td title="SpiderMan, Gamora, King Pin, DeadPool, Quake">82996</td>
-				<td title="Ant-man, Rocket Racoon, Thanos, Cable, Night Nurse">74081</td>
-				<td title="Captain America, Hawkeye, DareDevil, Wolverine, Yondu">65360</td>
+				<td>King Vic</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+				<tr>
+				<td>mKizzy</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+				<tr>
+				<td>DanimePK</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+			<tr>
+				<td>PaulThaAsian</td>
+				<td >82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+
+		</tbody>
+	</table>
+		<div class="team_lead">
+		<h3>Team Leads: </h3>
+		<p>Commander740, </p>
+		<p>imthatguy</p>
+	</div>
+
+	<table id="team-three">
+		<thead>
+			<th>Name</th>			
+			<th>Alpha Team STP</th>
+			<th>Beta Team STP</th>
+			<th>Gamma Team STP</th>
+			<th>Thanos Raid Lane</th>
+			<th>Ulitmis Raid 5 Lane </th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Commander740</td>
+				<td>88886</td>
+				<td>73075</td>
+				<td>62327</td>
+			</tr>
+			<tr>
+				<td>Imthatguy</td>
+				<td>83101</td>
+				<td>76011</td>
+				<td>61670</td>
+				<td></td>
+				<td></td>
+			</tr>
+				<tr>
+				<td>Ady</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+				<tr>
+				<td>King Vic</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+				<tr>
+				<td>mKizzy</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+				<tr>
+				<td>DanimePK</td>
+				<td>82996</td>
+				<td>74081</td>
+				<td>65360</td>
+			</tr>
+			<tr>
+				<td>PaulThaAsian</td>
+				<td >82996</td>
+				<td>74081</td>
+				<td>65360</td>
 			</tr>
 
 		</tbody>
